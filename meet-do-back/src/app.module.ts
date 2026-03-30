01bef4164803164c7e396c3f6c71fc5e11da2a82
zeyluish <teogdn.pro@gmail.com>
2026-03-30 09:29:32 +0200

    init back activity #2

--- meet-do-back/src/app.module.ts
+++ meet-do-back/src/app.module.ts
@@ -3,11 +3,13 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { SupabaseModule } from './supabase/supabase.module';
+import { ActivityModule } from './activity/activity.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     SupabaseModule,
+    ActivityModule,
   ],
   controllers: [AppController],
   providers: [AppService],
