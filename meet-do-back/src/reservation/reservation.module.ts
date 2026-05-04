c2bd0395cfa91ae8ede3a58c320a49e2e0720545
Téo G. <183733003+zeyluish@users.noreply.github.com>
2026-05-04 09:34:35 +0200

    feat(reservation): validate event capacity

--- meet-do-back/src/reservation/reservation.module.ts
+++ meet-do-back/src/reservation/reservation.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ReservationService } from './reservation.service';
 import { ReservationController } from './reservation.controller';
+import { SupabaseModule } from '../supabase/supabase.module';
 
 @Module({
+  imports: [SupabaseModule],
   controllers: [ReservationController],
   providers: [ReservationService],
 })
